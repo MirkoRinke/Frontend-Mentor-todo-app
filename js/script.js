e3a15a10aa4e2cd9bc70d8298549e22a7dde7c80
Mirko Rinke <mirkorinke@htp.com>
2024-10-07 00:20:26 +0200

    Refactor script.js: Add mobile menu rendering

--- js/script.js
+++ js/script.js
@@ -2,11 +2,12 @@ import { loadTodos } from "../components/storage/storage.js";
 import { getInput } from "../components/input/input.js";
 import { renderHeader } from "../components/header/header.js";
 import { renderInput } from "../components/input/input.js";
-import { renderMenu } from "../components/todoList/menu/menu.js";
+import { renderMenu, renderMenuMobile } from "../components/todoList/menu/menu.js";
 
 window.getInput = getInput;
 
 renderHeader();
 renderInput();
 renderMenu();
+renderMenuMobile();
 loadTodos();
